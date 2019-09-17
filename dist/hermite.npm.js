bf034363ae338fe453f70aa31a301397d62277e1
Vilius <www.viliusl@gmail.com>
2019-09-17 16:14:54 +0300

    npm update

--- dist/hermite.npm.js
+++ dist/hermite.npm.js
@@ -1,6 +1,6 @@
 /**
  * hermite-resize - Canvas image resize/resample using Hermite filter with JavaScript.
- * @version v2.2.7
+ * @version v2.2.8
  * @link https://github.com/viliusle/miniPaint
  * @license MIT
  */
